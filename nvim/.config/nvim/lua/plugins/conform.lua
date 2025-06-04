ba0a3ec8d32c5bcbb7e2af726c9d550dd41f4c7b
Daniel Bittencourt <contactbiz.dan@gmail.com>
2025-06-05 06:35:53 +1000

    fix(neovim): only use format_diffs on save for csharp files.

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -15,7 +15,6 @@ return {
 
     -- using mini.diff to identify git hunks
     local diff = require("mini.diff")
-    local range_ignore_filetypes = { "lua" }
     local format_diffs = function()
       local data = diff.get_buf_data()
       if not data or not data.hunks then
@@ -23,16 +22,9 @@ return {
         return
       end
 
-      -- stylua range format mess up indent, so use full format for now
-      if vim.tbl_contains(range_ignore_filetypes, vim.bo.filetype) then
-        format_file()
-        return
-      end
       local ranges = {}
       for _, hunk in pairs(data.hunks) do
         if hunk.type ~= "delete" then
-          -- always insert to index 1 so format below could start from last hunk
-          -- which this sort didn't mess up range
           table.insert(ranges, 1, {
             start = { hunk.buf_start, 0 },
             ["end"] = { hunk.buf_start + hunk.buf_count, 0 },
@@ -76,10 +68,16 @@ return {
           stdin = false,
         },
       },
-      format_on_save = function()
+      format_on_save = function(bufnr)
+        local filetype = vim.bo[bufnr].filetype
+
         -- only format git hunks to avoid massive changes on old files
-        -- should this become unnecessary, just replace by format_file function
-        format_diffs()
+        -- it makes pull-requests hard to get reviewed
+        if filetype == "cs" then
+          format_diffs()
+        else
+          format_file()
+        end
       end,
     })
 
