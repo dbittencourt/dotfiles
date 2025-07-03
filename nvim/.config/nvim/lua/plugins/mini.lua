96c12227671336d558c302e5edd8613e6c0990ec
Daniel Bittencourt <contactbiz.dan@gmail.com>
2025-07-03 16:14:07 +1000

    feat(neovim): improve treesitter configuration

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -48,7 +48,27 @@ return {
       require("mini.surround").setup()
 
       -- advanced text objects
-      require("mini.ai").setup()
+      local ai = require("mini.ai")
+      ai.setup({
+        {
+          n_lines = 500,
+          custom_textobjects = {
+            o = ai.gen_spec.treesitter({ -- code block
+              a = { "@block.outer", "@conditional.outer", "@loop.outer" },
+              i = { "@block.inner", "@conditional.inner", "@loop.inner" },
+            }),
+            m = ai.gen_spec.treesitter({ -- method/function definition
+              a = "@function.outer",
+              i = "@function.inner",
+            }),
+            c = ai.gen_spec.treesitter({ -- class
+              a = "@class.outer",
+              i = "@class.inner",
+            }),
+            d = { "%f[%d]%d+" }, -- digits
+          },
+        },
+      })
 
       -- automatic pair completion
       require("mini.pairs").setup({
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -31,7 +31,7 @@ return {
         "markdown_inline",
         "bash",
         "vim",
-        "vimdoc", -- fix errors when using :help <anything>
+        "vimdoc",
         "python",
         "diff",
         "c_sharp",
@@ -47,20 +47,7 @@ return {
           node_decremental = "<bs>",
         },
       },
-    })
-
-    require("treesitter-context").setup({
-      max_lines = 3,
-      multiline_threshold = 1,
-      min_window_height = 20,
-    })
-
-    require("nvim-treesitter.configs").setup({
       textobjects = {
-        select = {
-          enable = true,
-          lookahead = true,
-        },
         swap = {
           enable = true,
           swap_next = {
@@ -82,24 +69,16 @@ return {
         },
         move = {
           enable = true,
-          -- add jumps in the jumplist
           set_jumps = true,
           goto_next_start = {
-            ["]f"] = {
-              query = "@call.outer",
-              desc = "Next function call start",
-            },
             ["]m"] = {
               query = "@function.outer",
               desc = "Next method/function def start",
             },
-            ["]c"] = { query = "@class.outer", desc = "Next class start" },
             ["]i"] = {
               query = "@conditional.outer",
               desc = "Next conditional start",
             },
-            ["]l"] = { query = "@loop.outer", desc = "Next loop start" },
-
             ["]z"] = {
               query = "@fold",
               query_group = "folds",
@@ -107,49 +86,43 @@ return {
             },
           },
           goto_next_end = {
-            ["]F"] = { query = "@call.outer", desc = "Next function call end" },
             ["]M"] = {
               query = "@function.outer",
               desc = "Next method/function def end",
             },
-            ["]C"] = { query = "@class.outer", desc = "Next class end" },
             ["]I"] = {
               query = "@conditional.outer",
               desc = "Next conditional end",
             },
-            ["]L"] = { query = "@loop.outer", desc = "Next loop end" },
           },
           goto_previous_start = {
-            ["[f"] = {
-              query = "@call.outer",
-              desc = "Prev function call start",
-            },
             ["[m"] = {
               query = "@function.outer",
               desc = "Prev method/function def start",
             },
-            ["[c"] = { query = "@class.outer", desc = "Prev class start" },
             ["[i"] = {
               query = "@conditional.outer",
               desc = "Prev conditional start",
             },
-            ["[l"] = { query = "@loop.outer", desc = "Prev loop start" },
           },
           goto_previous_end = {
-            ["[F"] = { query = "@call.outer", desc = "Prev function call end" },
             ["[M"] = {
               query = "@function.outer",
               desc = "Prev method/function def end",
             },
-            ["[C"] = { query = "@class.outer", desc = "Prev class end" },
             ["[I"] = {
               query = "@conditional.outer",
               desc = "Prev conditional end",
             },
-            ["[L"] = { query = "@loop.outer", desc = "Prev loop end" },
           },
         },
       },
+    })
+
+    require("treesitter-context").setup({
+      max_lines = 3,
+      multiline_threshold = 1,
+      min_window_height = 20,
     })
   end,
 }
