96530e269ff4c277c755da34ad6a0a6e66943a2a
Daniel Bittencourt <contactbiz.dan@gmail.com>
2025-07-04 18:57:59 +1000

    feat(neovim): disable arrow keys to have life harder

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -27,3 +27,9 @@ map.set(
 -- selection helpers
 map.set("n", "<leader>ya", "0ggvG$y", { desc = "Copy buffer content" })
 map.set("n", "<leader>yd", "0ggvG$d", { desc = "Delete buffer content" })
+
+-- disable arrow keys to make life harder
+map.set({ "n", "v", "i" }, "<up>", "<nop>", { noremap = true })
+map.set({ "n", "v", "i" }, "<down>", "<nop>", { noremap = true })
+map.set({ "n", "v", "i" }, "<left>", "<nop>", { noremap = true })
+map.set({ "n", "v", "i" }, "<right>", "<nop>", { noremap = true })
