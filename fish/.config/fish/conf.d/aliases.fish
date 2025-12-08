26b9f484fc67d913fd46a81e53be7774789b9a54
Daniel Bittencourt <contactbiz.dan@gmail.com>
2025-12-08 12:49:51 +1100

    feat(fish): add new git aliases

--- fish/.config/fish/conf.d/aliases.fish
+++ fish/.config/fish/conf.d/aliases.fish
@@ -1,8 +1,12 @@
 status is-interactive; or exit
 
 alias ta "tmux new -A -D -s dotfiles"
+
 # use bat as a replacement for cat
 alias cat "bat -p"
+
 alias gs "git status"
 alias gd "git diff"
 alias gds "git diff --staged"
+alias gr "git reset --hard"
+alias grs "git reset --soft"
