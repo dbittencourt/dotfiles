cdfc45404f2ea257d3cb74657d8fd764315991ba
Daniel Bittencourt <contactbiz.dan@gmail.com>
2026-02-06 18:07:52 +1100

    feat(neovim): update lsp config

--- nvim/.config/nvim/lsp/basedpyright.lua
+++ nvim/.config/nvim/lsp/basedpyright.lua
@@ -1,5 +1,3 @@
--- install with: npm install -g basedpyright
-
 ---@brief
 ---
 --- https://detachhead.github.io/basedpyright
@@ -41,8 +39,11 @@ return {
 		basedpyright = {
 			analysis = {
 				autoSearchPaths = true,
-				useLibraryCodeForTypes = true,
 				diagnosticMode = "openFilesOnly",
+				-- https://docs.basedpyright.com/latest/configuration/language-server-settings/
+				-- Explicitly setting `basedpyright.analysis.useLibraryCodeForTypes` is **discouraged** by the official docs.
+				-- Because it will override per-project configurations like `pyproject.toml`.
+				-- If left unset, its default value is `true`, and it can be correctly overridden by project config files.
 			},
 		},
 	},
