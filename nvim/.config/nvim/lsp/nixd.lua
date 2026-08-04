9d2ef4ef559a7970d7af6a8ce6a78591a3f089f4
Daniel Bittencourt <business@dbitt.com>
2026-08-04 15:18:13 +1000

    fix(neovim): fix bug with nixd lsp and fugitive buffers

--- nvim/.config/nvim/lsp/nixd.lua
+++ nvim/.config/nvim/lsp/nixd.lua
@@ -1,5 +1,15 @@
+local markers = { "flake.nix", ".git" }
+
 return {
 	cmd = { "nixd" },
 	filetypes = { "nix" },
-	root_markers = { "flake.nix", ".git" },
+	root_dir = function(bufnr, on_dir)
+		local uri = vim.uri_from_bufnr(bufnr)
+		if not vim.startswith(uri, "file://") then
+			return
+		end
+
+		local path = vim.uri_to_fname(uri)
+		on_dir(vim.fs.root(path, markers) or vim.fs.dirname(path))
+	end,
 }
